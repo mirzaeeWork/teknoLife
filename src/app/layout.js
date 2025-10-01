e899a2aad5a05254a173a643c98e3f513db666d3
samane mirzaee <mirzaeesamane.work@gmail.com>
2025-10-01 14:26:05 +0330

    fix logo and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,7 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
 export const metadata = {
-  title: "برتینا بزرگ‌ترین ارائه دهنده خدمات هاستینگ و دامنه و کلیک گوگل در ایران",
-  description: "برتینا ارائه دهنده‌ی هاست وردپرس، سرور مجازی، گواهی ssl، تبلیغ در گوگل (Ads)، میزبانی ایمیل ، خرید دامنه (دامین) و هاست ویندوزی و لینوکسی است.",
+  title: "تکنو لایف",
 };
 
 export default function RootLayout({ children }) {
